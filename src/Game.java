491750c2dde22ac8391fdefe9d7cebe3a0e26488
afu0706 <a20807506@gmail.com>
2020-11-28 21:25:58 +0800

    Game 大致架構

--- src/Game.java
+++ src/Game.java
@@ -1,30 +1,62 @@
 import java.io.*;
-import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.ByteBuffer;
+import java.util.Random;
 
 public class Game extends Thread {
-    Socket P1 = null;
-    Socket P2 = null;
-    InputStream P1_in = null;
-    InputStream P2_in = null;
-    OutputStream P1_out = null;
-    OutputStream P2_out = null;
+    int P1 = 0;
+    int P2 = 1;
+    Socket[] Play = new Socket[2];
+    InputStream[] in = new InputStream[2];
+    OutputStream[] out = new OutputStream[2];
+    boolean win = false;
+    String order = null;
+    int[] pos_x = new int[2]; // position
+    int[] pos_y = new int[2];
 
-    public Game(Socket player1, Socket player2) {
-        P1 = player1;
-        P2 = player2;
-        try {
-            P1_in   = P1.getInputStream();
-            P1_out  = P1.getOutputStream();
-            P2_in   = P2.getInputStream();
-            P2_out  = P2.getOutputStream();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }      
+    public enum Item { // mix with item type
+        nothing, treasure, arrow, wall
     }
 
-    public void run(){
+    public class map_type { // use to create map
+        int x;
+        int y;
+        int item_type; // 0 is nothing, 1 is win point,....
 
+        map_type(int x, int y, int type) {
+            this.x = x;
+            this.y = y;
+            item_type = type;
+        }
+    }
+
+    public Game(Socket player1, Socket player2) {
+        Play[P1] = player1;
+        Play[P2] = player2;
+        try {
+            in[P1] = player1.getInputStream();
+            in[P2] = player2.getInputStream();
+            out[P1] = player1.getOutputStream();
+            out[P2] = player2.getOutputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run() {
+        int turn_counter = (int) Math.random() * 2; // 0 is P1 turn , 1 is P2 turn
+        order = "run";// run, stop, win
+        try {
+            while (!win) {
+                out[turn_counter].write(order.getBytes());
+                //out[turn_counter].write(map);
+                
+                
+                turn_counter++;
+                turn_counter%=2;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
