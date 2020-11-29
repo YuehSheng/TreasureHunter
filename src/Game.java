c0164a7dfe3ef605537c998f3c9a106430b17449
afu0706 <a20807506@gmail.com>
2020-11-30 00:14:32 +0800

    game mode 更改

--- src/Game.java
+++ src/Game.java
@@ -10,9 +10,9 @@ public class Game extends Thread {
     int P2 = 1;
 
     public enum Item { // mix with item type
-        nothing, treasure, arrow, wall, increase,decrease // props*3 treasure*1
+        nothing, treasure, arrow, wall, increase, decrease // props*3 treasure*1
     }
-    
+
     String Roomname;
     Socket[] Play = new Socket[2];
     InputStream[] in = new InputStream[2];
@@ -24,24 +24,24 @@ public class Game extends Thread {
 
     // use to create map
     // use to send
-    byte[] map = new byte[42*42];
+    byte[] map = new byte[42 * 42];
 
-    public void createMap(){ //create map
-        for (int i = 0;i < 42;i++){
-            for(int j = 0;j < 42;j++){
-                map[42*i+j] = (byte) (Item.nothing.ordinal() & 0xff);
+    public void createMap() { // create map
+        for (int i = 0; i < 42; i++) {
+            for (int j = 0; j < 42; j++) {
+                map[42 * i + j] = (byte) (Item.nothing.ordinal() & 0xff);
             }
         }
         HashSet<Integer> randPos = new HashSet<>();
-        while(randPos.size() < 13){
-            randPos.add((int)(Math.random()*42*42));
+        while (randPos.size() < 13) {
+            randPos.add((int) (Math.random() * 42 * 42));
         }
         Object[] itemPos = randPos.toArray();
-        for(int i = 0;i < 13;i++){
-            int x = (Integer)itemPos[i] / 42;
-            int y = (Integer)itemPos[i] % 42;
-            System.out.println(x+" "+y);
-            switch (i){
+        for (int i = 0; i < 13; i++) {
+            int x = (Integer) itemPos[i] / 42;
+            int y = (Integer) itemPos[i] % 42;
+            System.out.println(x + " " + y);
+            switch (i) {
                 case 0:
                     map[42 * y + x] = (byte) (Item.treasure.ordinal() & 0xff);
                     break;
@@ -69,9 +69,7 @@ public class Game extends Thread {
         }
     }
 
-
-    
-    public Game(String Roomname,Socket player1, Socket player2) {
+    public Game(String Roomname, Socket player1, Socket player2) {
         this.Roomname = Roomname;
         Play[P1] = player1;
         Play[P2] = player2;
@@ -85,7 +83,7 @@ public class Game extends Thread {
         }
     }
 
-    public String find_direction(int x, int y){
+    public String find_direction(int x, int y) {
         int treasure_x = 0;
         int treasure_y = 0;
         int dirX = 0;
@@ -130,7 +128,7 @@ public class Game extends Thread {
 
     public void run() {
         int turn_counter = (int) (Math.random() * 2); // 0 is P1 turn , 1 is P2 turn
-        int[] wait_counter = new int[2]; //arrow stop counter
+        int[] wait_counter = new int[2]; // arrow stop counter
         wait_counter[P1] = 0;
         wait_counter[P2] = 0;
         int mode = 0;
@@ -145,29 +143,15 @@ public class Game extends Thread {
                 in[turn_counter].read(client_mode);
                 in[turn_counter].read(client_action);
                 mode = ByteBuffer.wrap(client_mode, 0, 4).getInt();
-                
-                switch(mode){
-                    case 0: // move 
-                        //client should send int x, int y
-                        //read move position
-                        //have to delete this position props
-                        int x = ByteBuffer.wrap(client_action,0,4).getInt();
-                        int y = ByteBuffer.wrap(client_action,4,4).getInt();
-                        map[42*y+x] = (byte) (Item.nothing.ordinal() & 0xff);
-                        if(wait_counter[(turn_counter+1)%2] == 0){
-                            order = "run";
-                        } else {
-                            wait_counter[(turn_counter + 1) % 2]--;
-                            order = "wait";
-                        }
-                        break;
-                    case 1: // search
-                        //read position
-                        //send direction
-                        x = ByteBuffer.wrap(client_action, 0, 4).getInt();
-                        y = ByteBuffer.wrap(client_action, 4, 4).getInt();
-                        String direction = find_direction(x, y);
-                        out[turn_counter].write(direction.getBytes());
+
+                switch (mode) {
+                    case 10: // move
+                        // client should send int x, int y
+                        // read move position
+                        // have to delete this position props
+                        int x = ByteBuffer.wrap(client_action, 0, 4).getInt();
+                        int y = ByteBuffer.wrap(client_action, 4, 4).getInt();
+                        map[42 * y + x] = (byte) (Item.nothing.ordinal() & 0xff);
                         if (wait_counter[(turn_counter + 1) % 2] == 0) {
                             order = "run";
                         } else {
@@ -175,23 +159,36 @@ public class Game extends Thread {
                             order = "wait";
                         }
                         break;
-                    case 2: // props
-                        //read item number
+                    case 11: // search
+                        // read position
+                        // send direction
+                        x = ByteBuffer.wrap(client_action, 0, 4).getInt();
+                        y = ByteBuffer.wrap(client_action, 4, 4).getInt();
+                        String direction = find_direction(x, y);
+                        out[turn_counter].write(direction.getBytes()); // send direction
+                        if (wait_counter[(turn_counter + 1) % 2] == 0) {
+                            order = "run";
+                        } else {
+                            wait_counter[(turn_counter + 1) % 2]--;
+                            order = "wait";
+                        }
+                        break;
+                    case 12: // props
+                        // read item number
 
                         break;
-                    case 3: // dig
-                        //read dig position
+                    case 13: // dig
+                        // read dig position
 
                         break;
-                    case 4: // wait : if order is stop client should send 'wait' back
+                    case 14: // wait : if order is stop client should send 'wait' back
                         break;
                 }
-                
-                
-                turn_counter = (turn_counter+1)%2;
+
+                turn_counter = (turn_counter + 1) % 2;
             }
         } catch (IOException e) {
-            //if someone out should send server to delete this room
+            // if someone out should send server to delete this room
             e.printStackTrace();
         }
     }
